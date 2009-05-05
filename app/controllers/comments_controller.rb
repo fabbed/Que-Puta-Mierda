0fbd3b4f3239f7cc54785902673f27f4ef6688c4
Fabian Dittrich <fabian.dittrich@gmail.com>
2009-05-05 22:32:19 +0200

    test

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,7 @@ class CommentsController < ApplicationController
 
   def create
     comment = Comment.new(params[:comment])
+    comment.user_id = current_user.id if current_user
     
     @story = Story.find_by_id(params[:story])
     @story.add_comment(comment)
