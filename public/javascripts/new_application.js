8d42e06da1b10855f42fb93165bdb1775169e1ba
Fabian Dittrich <fabian.dittrich@gmail.com>
2009-08-17 16:09:22 +0200

    zdtrhdgfhgfd

---
+++ public/javascripts/new_application.js
@@ -0,0 +1,18 @@
+function round_corners () {
+  // $(".round_corners").corners();
+  // $(".round_section").corners();
+  // $('.top_round_section').corners("top 10px");
+  // $('.story_wrapper .footer').corners("bottom 10px");
+  // $('.story_wrapper .round').corners("top 10px");
+
+  $('#header_login_section').corners("bottom 10px");
+
+
+}
+
+
+$(document).ready(function() { 
+  round_corners();
+
+
+}); 
