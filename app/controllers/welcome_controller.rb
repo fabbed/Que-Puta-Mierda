e352b86ba6495d206315936f42dcb4e9cc3aba62
Fabian Dittrich <fabian.dittrich@gmail.com>
2009-08-17 16:09:02 +0200

    fdsfdsfds

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,30 @@
 class WelcomeController < ApplicationController
 
+  before_filter :authenticate, :only => :authenticate
+  
 
   def index
     @stories = Story.newest_first.paginate(:page => params[:page], :per_page => STORIES_PER_PAGE)
     @tags = Story.tag_counts(:limit => 40)
   end
+
+
+  def new
+    @stories = Story.newest_first.paginate(:page => params[:page], :per_page => 10)
+    render :action => "new_design", :layout => "new_layout"
+  end
+
+
+
+
+  private
+  
+  def authenticate
+    authenticate_or_request_with_http_basic do |user_name, password|
+      user_name == "adolfo" && password == "test"
+    end
+  end
+
+
 
 end
--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -1,2 +1,32 @@
 module StoriesHelper
+
+  def thumbs_up_link(story)
+    
+    icon = (story.rated_top == 0) ? image_tag("new/icon_thumbs_up_not_active.png"): image_tag("new/icon_thumbs_up_active.png")
+    
+    
+    content_tag(:div, icon + content_tag(:span, story.rated_flop.to_s), :class => "up")
+
+    
+    # =image_tag "new/icon_thumbs_down_active.png"
+    # =
+    # =image_tag "new/icon_thumbs_down_not_active.png"
+    # =image_tag "new/icon_thumbs_up_not_active.png"          
+    
+  end
+
+
+  def thumbs_down_link(story)
+    
+    icon = (story.rated_flop == 0) ? image_tag("new/icon_thumbs_down_not_active.png"): image_tag("new/icon_thumbs_down_active.png")    
+    content_tag(:div, icon + content_tag(:span, story.rated_flop.to_s), :class => "down")    
+    # =image_tag "new/icon_thumbs_down_active.png"
+    # =image_tag "new/icon_thumbs_up_active.png"
+    # =image_tag "new/icon_thumbs_down_not_active.png"
+    # =image_tag "new/icon_thumbs_up_not_active.png"          
+    
+  end
+
+
+
 end
