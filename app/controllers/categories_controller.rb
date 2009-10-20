b7a855481032358523bf151466e76cd33e3eedfc
Fabian Dittrich <fabian.dittrich@gmail.com>
2009-10-20 16:13:42 +0200

    hgfd

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,17 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id].to_s)
-    @stories = @category.stories.paginate(:page => params[:page], :per_page => STORIES_PER_PAGE)
+    sort_order=case params[:ordenar_por]
+      when nil
+        @stories = @category.stories.tops.paginate(:page => params[:page], :per_page => STORIES_PER_PAGE)
+      when "putadas"
+        @stories = @category.stories.tops.paginate(:page => params[:page], :per_page => STORIES_PER_PAGE)
+      when "nuevas"
+        @stories = @category.stories.newest_first.paginate(:page => params[:page], :per_page => STORIES_PER_PAGE)        
+      when "cagadas"
+        @stories = @category.stories.flops.paginate(:page => params[:page], :per_page => STORIES_PER_PAGE)        
+    end
+    
   end
 
 end
