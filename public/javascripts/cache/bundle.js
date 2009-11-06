1b061a429b131b26aa27f73c1f1d7df1f6146623
Fabian Dittrich <fabian.dittrich@gmail.com>
2009-11-06 21:22:33 +0100

    fsfdfds

--- public/javascripts/cache/bundle.js
+++ public/javascripts/cache/bundle.js
@@ -13,7 +13,7 @@ clearInterval(self.id);var val=val||100;this.id=setInterval(this.internalCallbac
 
 function round_corners(){$('#header_login_section').corners("bottom 10px");$('.round_20').corners("20px");$('#new_story_wrapper').corners("20px");$('.round').corners("10px");}
 function submit_form(form){$("input:submit",form).after($("#spinner").show());$("input:submit",form).attr("disabled",true).attr("value","Enviando...................");form.submit();}
-$(document).ready(function(){round_corners();$(".tooltip").tooltip({delay:0,showURL:false})
+$(document).ready(function(){round_corners();$("#select_country").change(function(){this.form.submit();});$(".tooltip").tooltip({delay:0,showURL:false})
 $.timer(2000,function(timer){var fade_time=200;$("#new_history").animate({opacity:1.0},fade_time).animate({opacity:0.1},fade_time,function(){$("#new_history").animate({opacity:0.1},fade_time).animate({opacity:1.0},fade_time)})
 timer.stop();});$.timer(5000,function(timer){var fade_time=200;$("#new_history").animate({opacity:1.0},fade_time).animate({opacity:0.1},fade_time,function(){$("#new_history").animate({opacity:0.1},fade_time).animate({opacity:1.0},fade_time)})
 timer.stop();});});/* --------- /javascripts/jquery.form.js --------- */ 
